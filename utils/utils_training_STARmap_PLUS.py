d420ed8ae1b3762794eaa997907168414b93db41
Zachary <33209626+ZacharyWang-007@users.noreply.github.com>
2026-02-27 21:14:34 +0800

    Add files via upload

---
+++ utils/utils_training_STARmap_PLUS.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+import random
+import torch
+
+from utils.evaluation import evaluator
+
+from utils.utils import AverageMeter
+from sklearn.metrics import roc_auc_score, confusion_matrix
+
+
+def train(model, criterion, optimizer, trainloader, ct_information=False):
+    model.train()
+    losses = AverageMeter()
+    for batch_idx, (rna, protein, cell, rna_neighbors, cell_neighbor, _) in enumerate(trainloader):
+
+        rna, protein, rna_neighbors = rna.cuda(), protein.cuda(), rna_neighbors.cuda()
+        cell, cell_neighbor = cell.cuda(), cell_neighbor.cuda()
+        
+        ############
+        if random.random() > 0.7:
+            mask = torch.ones((rna_neighbors.size(0), rna_neighbors.size(1), 1))
+            mask = torch.bernoulli(torch.full(mask.shape, 0.5)).cuda()
+            rna_neighbors = rna_neighbors * mask
+            cell_neighbor = cell_neighbor * mask
+        ############
+
+        cell_inf = torch.cat([cell[:, None, :], cell_neighbor], dim=1)
+        source, target, source_neightbors = rna, protein, rna_neighbors
+        
+        if ct_information == True:
+            outputs = model(source, source_neightbors, cell_inf)
+        else: 
+            outputs = model(source, source_neightbors)
+        outputs = torch.sigmoid(outputs)
+
+        loss = criterion(outputs, target) 
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.update(loss.data, source.size(0))
+
+        if (batch_idx+1) == len(trainloader):
+            print("Batch {}/{}\t Loss {:.6f} ({:.6f})".format(batch_idx+1, len(trainloader), losses.val, losses.avg))
+
+
+def test(model, testloader, ct_information=False):
+    model.eval()
+
+    predict_list, target_list = [], []
+    
+    with torch.no_grad():
+        for _, (source, target, cell, source_neightbors, cell_neighbor, _) in enumerate(testloader):
+
+            source, target, source_neightbors = source.cuda(), target.cuda(), source_neightbors.cuda()
+            cell_inf = torch.cat([cell[:, None, :], cell_neighbor], dim=1).cuda()
+
+            if ct_information == True:
+                outputs = model(source, source_neightbors, cell_inf)
+            else: 
+                outputs = model(source, source_neightbors)
+            outputs = torch.sigmoid(outputs)
+            
+            predict_list.append(outputs)
+            target_list.append(target)
+
+    predict_list = torch.cat(predict_list, dim=0).cpu().numpy()
+    target_list = torch.cat(target_list, dim=0).cpu().numpy()
+    
+    ##############
+    auc_tau = roc_auc_score(target_list[:, 0], predict_list[:, 0])
+    tn, fp, fn, tp = confusion_matrix(target_list[:, 0], (predict_list[:, 0] > 0.5).astype(int)).ravel()
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+    print(f"tau AUC: {auc_tau}, tau sensitivity {sensitivity}, tay specificity {specificity}")
+
+    ##############
+    auc_plaque = roc_auc_score(target_list[:, 1], predict_list[:, 1])
+    tn, fp, fn, tp = confusion_matrix(target_list[:, 1], (predict_list[:, 1] > 0.5).astype(int)).ravel()
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+    print(f"Aβ AUC: {auc_plaque}, Aβ sensitivity {sensitivity}, Aβ specificity {specificity}")
+
+    return None
---
+++ utils/utils_training_human_lymph_node.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+import random
+import torch
+
+from utils.evaluation import evaluator
+from utils.utils import AverageMeter
+
+
+def train(model, criterion, optimizer, trainloader):
+    model.train()
+    losses = AverageMeter()
+
+    for batch_idx, (rna, protein, rna_neighbors, _) in enumerate(trainloader):
+
+        rna, protein, rna_neighbors = rna.cuda(), protein.cuda(), rna_neighbors.cuda()
+
+        ############
+        if random.random() > 0.7:
+            mask = torch.ones((rna_neighbors.size(0), 8, 1))
+            mask = torch.bernoulli(torch.full(mask.shape, 0.5)).cuda()
+            rna_neighbors = rna_neighbors * mask
+        ############
+
+        source, target, source_neightbors = rna, protein, rna_neighbors
+        outputs = model(source, source_neightbors)
+        loss = criterion(outputs, target) 
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.update(loss.data, source.size(0))
+
+        if (batch_idx+1) == len(trainloader):
+            print("Batch {}/{}\t Loss {:.6f} ({:.6f})".format(batch_idx+1, len(trainloader), losses.val, losses.avg))
+
+
+def test(model, testloader):
+    model.eval()
+
+    predict_list, target_list = [], []
+    
+    
+    with torch.no_grad():
+        for _, (source, target, source_neightbors, _) in enumerate(testloader):
+
+            source, target, source_neightbors = source.cuda(), target.cuda(), source_neightbors.cuda()
+            outputs = model(source, source_neightbors)
+
+            predict_list.append(outputs)
+            target_list.append(target)
+
+
+    
+    pearson_sample_list, spearman_sample_list, rmse_list = evaluator(predict_list, target_list)
+    pearson_mean, spearman_mean, rmse_mean = pearson_sample_list.mean(), spearman_sample_list.mean(), rmse_list.mean()
+    pearson_std, spearman_std, rmse_std = pearson_sample_list.std(), spearman_sample_list.std(), rmse_list.std()
+
+    print('Testing Set: pearson correlation {:.4f} + {:.4f}; spearman correlation {:.4f} + {:.4f}; rmse {:.4f} + {:.4f}'
+                                            .format(pearson_mean, pearson_std, spearman_mean, spearman_std, rmse_mean, rmse_std))
+    
+    return pearson_mean
